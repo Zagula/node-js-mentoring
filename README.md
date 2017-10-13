7c7f073febf42c40576dacaa01a79f903b4c77ac
Andrei_Tsiarnouski <Andrei_Tsiarnouski@epam.com>
2017-10-13 17:35:45 +0300

    Completed

--- README.md
+++ README.md
@@ -5,9 +5,14 @@
 ```
 npm run hw1
 ```
-#### 2. Nodemon/Watcher (current)*
+#### 2. Nodemon/Watcher
 ```
 npm run hw2
-``````
+```
+#### 3. CommandLine/Streams (current)*
+```
+npm run hw3
+npm run streams -- [...args]
+```
 
 *\***current** hometask can also be executing by `npm start`*
--- modules/utils.js
+++ modules/utils.js
@@ -1,29 +1,26 @@
 'use strict';
 
-const help = {
+export default {
+    csvtojson: function(text) {
+        let lines = text.split('\n');
+        console.log(lines[0]);
+        let headers = this.split(lines[0]);
+        let result = [];
+
+        for (let i = 1; i < lines.length - 1; i++) {
+            let line = this.split(lines[i]);
+            let object = {};
+            for (let j = 0; j < headers.length; j++) {
+                object[headers[j]] = this.trim(line[j]);
+            }
+            result.push(object);
+        }
+        return result;
+    },
     split: (string) => {
         return string.match(/((["'])[^\2]*?\2)|([^'",]+)|(?=^,)|(?=,$)|(?=,,)/g);
     },
     trim: (string = '') => {
         return string.replace(/^(["'])([^\1]*)\1/, "$2");
-    }
-}
-
-export default {
-    csvtojson: function(text) {
-        let lines = text.split('\n');
-        console.log(lines[0]);
-        let headers = help.split(lines[0]);
-        let result = [];
-
-        for (let i = 1; i < lines.length - 1; i++) {
-            let line = help.split(lines[i]);
-            let object = {};
-            for (let j = 0; j < headers.length; j++) {
-                object[headers[j]] = help.trim(line[j]);
-            }
-            result.push(object);
-        }
-        return result;
     }
 }
